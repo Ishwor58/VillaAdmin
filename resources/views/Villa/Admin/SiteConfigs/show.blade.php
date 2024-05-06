8f727b9246b158fa7a31eab060fab22583f0d7b8
ishwor58 <ishworgurung2058@gmail.com>
2024-05-06 13:53:07 +0545

    corrected index and show in siteconfigs

--- resources/views/Villa/Admin/SiteConfigs/show.blade.php
+++ resources/views/Villa/Admin/SiteConfigs/show.blade.php
@@ -6,24 +6,24 @@
 
 <div class="content-wrapper">
     <div class="container-xxl flex-grow-1 container-p-y">
-        <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">fact/</span> Details</h4>
+        <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">SiteConfigs/</span> Details</h4>
 
         <div class="card mb-4">
             <div class="card-header">
-                <h5 class="mb-0">fact Details</h5>
+                <h5 class="mb-0">SiteConfigs Details</h5>
             </div>
             <div class="card-body">
                 
                 <div class="row mb-3">
-                    <label class="col-sm-2 col-form-label">Title:</label>
+                    <label class="col-sm-2 col-form-label">Key:</label>
                     <div class="col-sm-10">
-                        <p>{{ $siteconfig->title }}</p>
+                        <p>{{ $siteconfig->site_key }}</p>
                     </div>
                 </div>
                 <div class="row mb-3">
-                   <label class="col-sm-2 col-form-label">Sub-title:</label>
+                   <label class="col-sm-2 col-form-label">Value:</label>
                     <div class="col-sm-10">
-                        <p>{{ $siteconfig->sub_title }}</p>
+                        <p>{{ $siteconfig->site_value }}</p>
                     </div>
                 </div>
             </div>
@@ -31,7 +31,7 @@
 
         <div class="row justify-content-end">
             <div class="col-sm-10">
-                <a href="{{ route('facts.index') }}" class="btn btn-primary">Back to List</a>
+                <a href="{{ route('siteconfigs.index') }}" class="btn btn-primary">Back to List</a>
             </div>
         </div>
     </div>
